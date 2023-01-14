d7b1f0de46384514b7d19f512011725c15475a58
PQTATOS <mrhotdog@yandex.ru>
2023-01-14 16:12:56 +0500

    FogOfWar added

--- EVO/Assets/Scripts/FogWar_manager.cs
+++ EVO/Assets/Scripts/FogWar_manager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class FogWar_manager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject fog;
+    public int startxCord = -185;
+    public int startyCord = 235;
+    public int offSet = 55;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int x = 0; x < 9; x++)
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                if (startxCord + x * offSet != 0 || startyCord - y * offSet != 1)
+                {
+                    var newPos = new Vector2(startxCord + x * offSet, startyCord - y * offSet);
+                    Instantiate(fog, newPos, Quaternion.identity);
+                }
+            }
+        }
     }
 }
